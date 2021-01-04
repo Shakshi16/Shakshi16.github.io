a4a30c82526ae41113d7641600bc9a7739d94936
Shakshi16 <55783694+Shakshi16@users.noreply.github.com>
2021-01-04 22:31:55 +0530

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,12 @@
+// $(document).ready(function(){
+// let hamberger = document.querySelector('.hamberger');
+// let cross = document.querySelector('.cross');
+// let mobileNav = document.querySelector('.mobile-nav');
+
+// hamberger.addEventListener('click', function(){
+//     mobileNav.classList.add('open');
+// });
+// cross.addEventListener('click', function(){
+//     mobileNav.classList.remove('open');
+// });
+// })
